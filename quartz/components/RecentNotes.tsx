ff543146f941415e773b6ac26c96732d6ede5884
shaggy2922 <12015958+evmcnb@users.noreply.github.com>
2024-03-11 18:13:17 +0000

    remove documentation

--- quartz/components/RecentNotes.tsx
+++ quartz/components/RecentNotes.tsx
@@ -45,18 +45,18 @@ export default ((userOpts?: Partial<Options>) => {
               <li class="recent-li">
                 <div class="section">
                   <div class="desc">
-                    <h3>
+                    <h6>
                       <a href={resolveRelative(fileData.slug!, page.slug!)} class="internal">
                         {title}
                       </a>
-                    </h3>
+                    </h6>
                   </div>
-                  {page.dates && (
+                </div>
+                {page.dates && (
                     <p class="meta">
                       <Date date={getDate(cfg, page)!} locale={cfg.locale} />
                     </p>
                   )}
-                </div>
               </li>
             )
           })}
